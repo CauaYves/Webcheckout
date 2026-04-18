636b98b10a541042e18b3b36ffb61659222dc382
CauaYves <cauayves5@gmail.com>
2026-04-18 12:02:52 -0300

    feat: setup biomejs formatter and linting all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
-
-  return (
-   <body>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi recusandae exercitationem debitis sapiente cupiditate, fugit porro! Rerum ipsa nostrum fugit odit libero quisquam reiciendis corrupti aspernatur delectus, minus dolorum?
-   </body>
-  )
+	return (
+		<body>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi
+			recusandae exercitationem debitis sapiente cupiditate, fugit porro! Rerum
+			ipsa nostrum fugit odit libero quisquam reiciendis corrupti aspernatur
+			delectus, minus dolorum?
+		</body>
+	);
 }
 
-export default App
+export default App;

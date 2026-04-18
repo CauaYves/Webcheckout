a77da1a957958b1dba8abca0afc0c3284efa355c
CauaYves <cauayves5@gmail.com>
2026-04-18 10:48:40 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+   <body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque sequi recusandae exercitationem debitis sapiente cupiditate, fugit porro! Rerum ipsa nostrum fugit odit libero quisquam reiciendis corrupti aspernatur delectus, minus dolorum?
+   </body>
+  )
+}
+
+export default App
